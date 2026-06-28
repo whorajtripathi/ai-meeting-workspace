d544ed163fc63133b435fcaeee3acd1613acba98
whorajtripathi <rajsanjaykumartripathi@gmail.com>
2026-06-29 01:24:57 +0530

    Added User Repository

--- server/src/modules/users/user.repository.ts
+++ server/src/modules/users/user.repository.ts
@@ -3,3 +3,45 @@
 // Create User
 // Update User
 // Delete User
+
+import { User } from "./user.model";
+import { IUser } from "./user.types";
+
+export class UserRepository{
+
+    async createUser(userData: IUser){
+        return await User.create(userData);         //mongodb.create(type)
+    }
+
+    // Finding user by email.
+    async findByEmail(email:string){
+        return await User.findOne({
+            email,
+        }).select("+password");                     //earlier we wrote-> password: {select:false}  so it temporaryly includes password
+    }
+
+    // Find user by ID
+    async findById(id:string){
+        return await User.findById(id);
+    }
+
+    // Update user.
+    async updateUser(id:string, updateData:Partial<IUser>){
+        return await User.findByIdAndUpdate(
+            id,
+            updateData,
+            {
+                new:true,
+            }
+        );
+    }
+
+    // Delete user.
+    async deleteUser(id:string){
+        return await User.findByIdAndDelete(id);
+    }
+
+}
+
+export const userRepository=new UserRepository();
+
