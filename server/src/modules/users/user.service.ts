9388636fcd42703c4ac8c6f33567ef63626927ce
whorajtripathi <rajsanjaykumartripathi@gmail.com>
2026-06-29 01:52:14 +0530

    Added User Services

--- server/src/modules/users/user.service.ts
+++ server/src/modules/users/user.service.ts
@@ -4,3 +4,27 @@
 // Generate JWT
 // Send Verification Email
 
+
+import bcrypt from "bcrypt";
+import { IUser } from "./user.types";
+import { userRepository, UserRepository } from "./user.repository";
+
+export class UserService{
+
+    async register(userData: IUser){
+
+        const existingUser= await userRepository.findByEmail(userData.email);
+
+        if(existingUser){
+            throw new Error("Email already exists");
+        }
+
+        const hashPassword=await bcrypt.hash(userData.password,10);
+
+        userData.password=hashPassword;
+
+        return await userRepository.createUser(userData);
+    }
+}
+
+export const userService= new UserService();
